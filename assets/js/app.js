815dd875b163d65cae7e0b4b387758667028e474
Darwel Quintero <darwel34@gmail.com>
2019-06-04 18:23:48 -0430

    se agrego la logica de exito o error al enviar en mensaje de correo

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,25 @@ $(document).foundation();
 (function(){
   emailjs.init("user_sa0pTGfc3hpt7Sp6Iv7nw");
 })();
-$("#send").click(function(event){
-  event.preventDefault();
+$("#email_form").on('formvalid.zf.abide', function(e) {
+  e.preventDefault();
+  console.log("valid trigger");
   emailjs.sendForm('yahoo', 'template_contacto', '#email_form')
   .then(function(response) {
      console.log('SUCCESS!', response.status, response.text);
+     Swal.fire({
+      title:'Mensaje enviado!',
+      message:'Haga clic en el boton OK para continuar',
+      timer: 3000,
+      type:'success'
+     });
   }, function(error) {
      console.log('FAILED...', error);
+     Swal.fire({
+      title:'Mensaje no enviado!',
+      message:'Haga clic en el boton OK para continuar',
+      type:'error',
+      timer: 3000
+     });
   });
-
 });
-
