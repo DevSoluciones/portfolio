5b8519c222077fbb4415f7c2cc824dbd30a9140e
Darwel Quintero <darwel34@gmail.com>
2019-06-04 14:04:25 -0430

    se incluyo la logica de envio de correo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
-$(document).foundation()
+$(document).foundation();
+(function(){
+  emailjs.init("user_sa0pTGfc3hpt7Sp6Iv7nw");
+})();
+$("#send").click(function(event){
+  event.preventDefault();
+  emailjs.sendForm('yahoo', 'template_contacto', '#email_form')
+  .then(function(response) {
+     console.log('SUCCESS!', response.status, response.text);
+  }, function(error) {
+     console.log('FAILED...', error);
+  });
+
+});
+
